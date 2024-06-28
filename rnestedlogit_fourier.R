5b6d70ad7449001f9d4cb61b5ed202316cf9f85d
Wilbur <wilbur.townsend@gmail.com>
2024-06-28 17:48:54 +1200

    Update rnestedlogit_fourier.R

--- rnestedlogit_fourier.R
+++ rnestedlogit_fourier.R
@@ -1,5 +1,5 @@
 # Draw GEV random variables of the nested logit form:
-#	P[X < x] = exp(∑_{c∈C} (∑_{f∈C} exp(-x_f/(1-σ))^(1-σ))
+#	P[X < x] = exp(∑_{c∈C} (∑_{f∈c} exp(-x_f/(1-σ))^(1-σ))
 #
 # To run:
 # 	source("/Users/wtownsend/Documents/GitHub/r-nested-logit/rnestedlogit_fourier.R")
@@ -10,13 +10,18 @@ library(pracma)
 # σ should be a scalar.
 # nests should be a length-product vector of each product's nest.
 rnestedlogit = function(N, σ, nests, tol=1e-2, plot_pdf=FALSE) {
-	# Check nests is a vector of positive integers, with none missing.
+	# Check nests is a vector of integers.
 	stopifnot(is.vector(nests))
 	stopifnot(all(as.integer(nests) == nests))
-	stopifnot(all(nests >= 1))
-	num_nests    = max(nests)
-	num_products = length(nests)
-	stopifnot(num_nests == length(unique(nests)))
+	# Assign each nest to a sequential integer.
+	num_nests    = length(unique(nests))
+	xwalk = cbind(unique(nests), 1:num_nests)
+	new_nests = sapply(nests, function(nest) xwalk[which(xwalk[,1] == nest), 2])
+	# Check that...
+	stopifnot(all(new_nests >= 1))
+	stopifnot(num_nests == max(new_nests))
+	# Count products
+	num_products = length(new_nests)
 	# Define the characteristic function.
 	characteristic = function(z) gammaz(1 - 1i*z)/gammaz(1 - 1i*(1-σ)*z)
 	# Integrate to find the PDF -- should check these parameters.
@@ -35,7 +40,7 @@ rnestedlogit = function(N, σ, nests, tol=1e-2, plot_pdf=FALSE) {
 	# Integrate over the pdf to find the CDF.
 	out$cdf = cumsum(out$values_censored)
 	out$cdf = out$cdf/out$cdf[resolution]
-	# Draw.
+	# Draw using inverse transform sampling, interpolating the inverse CDF.
 	u = runif(N*num_nests)
 	index_before = sapply(u, function(ui) which(out$cdf < ui)[sum(out$cdf < ui)] )
 	index_after  = index_before + 1
@@ -43,24 +48,24 @@ rnestedlogit = function(N, σ, nests, tol=1e-2, plot_pdf=FALSE) {
 	cdf_after  = out$cdf[index_after]
 	weight_on_after = (u - cdf_before)/(cdf_after - cdf_before)
 	ζ = weight_on_after*out$w[index_after] + (1-weight_on_after)*out$w[index_before]
-	# Allocate over draws.
+	# Allocate ζ over draws.
 	ζ_reshape = matrix(ζ, nrow=N, ncol=num_nests)
 	# for some reason this doesn't work??
-	#		outer(1:N, 1:num_products, function(draw, p) ζ_reshape[draw, nests[p]])
+	#		outer(1:N, 1:num_products, function(draw, p) ζ_reshape[draw, new_nests[p]])
 	ζ_allocated = matrix(nrow=N, ncol=num_products)
 	for (p in 1:num_products) {
-		ζ_allocated[,p] = ζ_reshape[,nests[p]]
+		ζ_allocated[,p] = ζ_reshape[,new_nests[p]]
 	}
 	# Now just add the standard GEV.
 	gumbels = matrix(rgumbel(num_products*N), nrow=N)
-	return(gumbels + ζ_allocated)
+	return((1-σ)*gumbels + ζ_allocated)
 }
 
 
 set.seed(1)
 
 # Test.
-N = 1000000
+N = 100000
 num_nests = 4
 F_per_nest = 1 + rpois(num_nests, 5)
 nests = do.call(c, lapply(1:num_nests, function(n) matrix(n, ncol=F_per_nest[n])))
@@ -88,5 +93,29 @@ group_probs_analytic = nest_value^(1-σ)/sum(nest_value^(1-σ))
 # Compare.
 print(cbind(conditional_props, conditional_props_analytic))
 print(cbind(group_props, group_probs_analytic))
+
+
+
+
+
+
+# Check my alt convention.
+λ = 1-σ
+u_alt = u/λ
+δ_alt = delta/λ
+v_alt = u_alt + t(δ_alt)%x%matrix(1, nrow=N)
+f_alt = sapply(1:N, function(i) which.max(v_alt[i,]))
+
+props = sapply(1:F, function(i) mean(f_alt == i))
+group_props = sapply(f_by_nest, function(nest) sum(props[nest]))
+group_props_of_f = group_props[nests]
+conditional_props = props/group_props_of_f
+
+nest_value = sapply(f_by_nest, function(nest) sum(exp(δ_alt[nest])))
+conditional_props_analytic = exp(delta)/nest_value[nests]
+group_probs_analytic = nest_value^(λ)/sum(nest_value^(λ))
+print(cbind(conditional_props, conditional_props_analytic))
+print(cbind(group_props, group_probs_analytic))
+
 
 
